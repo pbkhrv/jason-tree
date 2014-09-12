c882bd842e6570670b9eb513e27cb8c46b561007
Peter Bakhirev <petro.b@gmail.com>
2014-09-12 18:28:32 -0400

    node creation events working

--- jasontree-core-tests.js
+++ jasontree-core-tests.js
@@ -1,6 +1,7 @@
 var jt = require('./jasontree-core.js');
+var lodash = require('lodash');
 
-describe('simple GET operation', function() {
+describe('Query data by path in the tree', function() {
   beforeEach(function() {
     this.tree = jt.tree({
       users: {
@@ -15,35 +16,35 @@ describe('simple GET operation', function() {
     });
   });
 
-  it('should get primitives', function() {
+  it('can retrieve primitives', function() {
     var firstName = jt.get(this.tree, '/users/bobby/firstName');
     expect(firstName).toEqual('Bob');
   });
 
-  it('should get objects', function() {
+  it('can retrieve objects', function() {
     var user = jt.get(this.tree, '/users/bobby');
     expect(user).toEqual({
       firstName: 'Bob'
     });
   });
 
-  it('should get arrays', function() {
+  it('can retrieve arrays', function() {
     var books = jt.get(this.tree, '/users/alice/books');
     expect(books).toEqual(['Alice in Wonderland']);
   });
 
-  it('should be safe to access non-existent nodes', function() {
+  it('is safe to access non-existent nodes', function() {
     var noSuchThing = jt.get(this.tree, '/no/such/path/in/this/tree');
     expect(noSuchThing).not.toBeDefined();
   });
 
-  it('should accept path as an array of key names', function() {
+  it('can accept path as an array of key names', function() {
     var books = jt.get(this.tree, ['users', 'alice', 'books']);
     expect(books).toEqual(['Alice in Wonderland']);
   });
 });
 
-describe('simple PUT operations', function() {
+describe('Insert data at some path', function() {
   beforeEach(function() {
     this.tree = jt.tree();
     this.getTree = function(path) {
@@ -51,19 +52,19 @@ describe('simple PUT operations', function() {
     };
   });
 
-  it('should insert primitives', function() {
+  it('can insert primitives', function() {
     jt.put(this.tree, '/users/bobby/firstName', 'Bob');
     expect(this.getTree('/users/bobby/firstName')).toEqual('Bob');
   });
 
-  it('should insert objects', function() {
+  it('can insert objects', function() {
     jt.put(this.tree, '/users/bobby', {
       firstName: 'Bob'
     });
     expect(this.getTree('/users/bobby/firstName')).toEqual('Bob');
   });
 
-  it('should insert arrays', function() {
+  it('can insert arrays', function() {
     jt.put(this.tree, '/users/alice/books', ['Alice in Wonderland']);
     expect(this.getTree('/users/alice/books')).toEqual(['Alice in Wonderland']);
   });
@@ -81,14 +82,21 @@ describe('simple PUT operations', function() {
     expect(overwrite).toThrow();
   });
 
-  it('should insert primitive into root of the tree', function() {
+  it('can insert primitives on any level in the tree above root', function() {
     jt.put(this.tree, '/count', 5);
     expect(this.getTree('/count')).toEqual(5);
   });
+
+  it('can insert leaf nodes on existing paths', function() {
+    jt.put(this.tree, '/users', {});
+    jt.put(this.tree, '/users/bobby', {});
+    jt.put(this.tree, '/users/bobby/firstName', 'Bob');
+    expect(this.getTree('/users/bobby/firstName')).toEqual('Bob');
+  });
 });
 
-describe('Generation of unique keys when inserting data', function() {
-  it('should generate unique id for $$uuid$$ in path', function() {
+describe('Generate unique keys when inserting data', function() {
+  it('generates unique id when $$uuid$$ is mentioned in path', function() {
     var tree = jt.put(jt.tree(), '/users/$$uuid$$', {
       firstName: 'Bob',
       lastName: 'Smith'
@@ -98,7 +106,7 @@ describe('Generation of unique keys when inserting data', function() {
     expect(jt.get(tree, ['users', userId, 'firstName'])).toEqual('Bob');
   });
 
-  it('can accept multiple $$uuid$$ in path', function() {
+  it('generates different unique id for each $$uuid$$ in path', function() {
     var tree = jt.put(jt.tree(), '/users/$$uuid$$/books/$$uuid$$', {
       name: 'Alice in Wonderland',
       author: 'Lewis Carroll'
@@ -224,7 +232,7 @@ describe('Array operations', function() {
   });
 });
 
-describe('Node creation events', function() {
+describe('Callback when new node is created', function() {
   beforeEach(function(done) {
     var tree = jt.tree({
       users: {
@@ -232,16 +240,168 @@ describe('Node creation events', function() {
         }
       }
     });
-    tree.doneNotifyingCb = done;
 
-    this.createdCallback = function(path) {};
-    spyOn(this, 'createdCallback');
+    this.bobbyFirstNameCb = jasmine.createSpy('bobbyFirstNameCb');
+    this.anyNewUserCb = jasmine.createSpy('anyNewUserCb');
+    this.anyLastNameCb = jasmine.createSpy('anyLastNameCb');
 
-    jt.on(tree, 'treeNodeCreated', this.createdCallback);
+    jt.on(tree, 'put', '/users/bobby/firstName', this.bobbyFirstNameCb);
+    jt.on(tree, 'put', '/users/$$anyKey$$', this.anyNewUserCb);
+    jt.on(tree, 'put', '$$anyPath$$/lastName', this.anyLastNameCb);
+
+    tree.doneNotifyingCb = lodash.after(2, done);
+
     jt.put(tree, '/users/bobby/firstName', 'Bob');
+    jt.put(tree, '/users/alice/lastName', 'Smith');
   });
 
-  it('should call when a primitive is inserted', function() {
-    expect(this.createdCallback).toHaveBeenCalledWith(['users', 'bobby', 'firstName']);
+  it('is called when new node matches path', function() {
+    expect(this.bobbyFirstNameCb).toHaveBeenCalledWith('/users/bobby/firstName', 'Bob');
+  });
+
+  it('is called when new node matches path pattern with $$anyKey$$', function() {
+    expect(this.anyNewUserCb).toHaveBeenCalledWith('/users/alice', {lastName: 'Smith'});
+  });
+
+  it('is called when new node matches path pattern with $$anyPath$$', function() {
+    expect(this.anyLastNameCb).toHaveBeenCalledWith('/users/alice/lastName', 'Smith');
+  });
+});
+
+xdescribe('Callbacks when object is inserted', function() {
+  beforeEach(function(done) {
+    var tree = jt.tree({});
+    this.spyPut = jasmine.createSpy('spyPut');
+    jt.on(tree, 'put', '/$$anyPath$$', this.spyPut);
+    jt.on(tree, 'put', '/$$anyPath$$', console.log);
+    tree.doneNotifyingCb = done;
+
+    this.obj = {
+      bobby: {
+        friends: [
+          {
+            name: 'Somebody'
+          },
+          {
+            name: 'Alice'
+          }
+        ]
+      }
+    };
+
+    jt.put(tree, '/users', this.obj);
+  });
+
+  it('called for each part of the path when pattern is set to anyPath', function() {
+    expect(this.spyPut).toHaveBeenCalledWith('/users', this.obj);
+    expect(this.spyPut).toHaveBeenCalledWith('/users/bobby', this.obj.bobby);
+    expect(this.spyPut).toHaveBeenCalledWith('/users/bobby/friends', this.obj.bobby.friends);
+    expect(this.spyPut).toHaveBeenCalledWith('/users/bobby/friends/$$last$$', this.obj.bobby.friends[1]);
+    expect(this.spyPut).toHaveBeenCalledWith('/users/bobby/friends/$$last$$/name', this.obj.bobby.friends[1].name);
+  });
+});
+
+describe('Promise tied to new node creation', function() {
+  beforeEach(function(done) {
+    var tree = jt.tree({
+      users: {
+      }
+    });
+
+    this.firstNameCb = jasmine.createSpy('firstNameCb');
+
+    jt.when(tree, 'put', '/users/$$anyKey$$/firstName').then(function(value) {
+      this.firstNameCb(value);
+    }.bind(this));
+
+    tree.doneNotifyingCb = lodash.after(2, done);
+
+    jt.put(tree, '/users/$$uuid$$/firstName', 'Bob');
+    jt.put(tree, '/users/$$uuid$$/firstName', 'Alice');
+  });
+
+  xit('is resolved once', function() {
+    expect(this.firstNameCb.calls.count()).toEqual(1);
+  });
+
+  xit('is resolved and is passed one of the inserted values', function() {
+    expect(this.firstNameCb.calls.mostRecent().args(0)).toMatch(/Bob|Alice/);
+  });
+});
+
+describe('Path pattern matching logic', function() {
+  it('should match path without globs', function() {
+    rex = jt.regexFromPathPattern("/this/is/a/path");
+    expect("/this/is/a/path".match(rex)).toBeTruthy();
+    expect("/this/is/a/path/different".match(rex)).toBeFalsy();
+  });
+
+  it('should match /end/of/path/$$anyKey$$ at the end of the path', function() {
+    rex = jt.regexFromPathPattern("/end/of/path/$$anyKey$$");
+    // match
+    expect("/end/of/path/someKey".match(rex)).toBeTruthy();
+    // no match
+    expect("/end/of/path/what/someKey".match(rex)).toBeFalsy();
+  });
+
+  it('should match /user/$$anyKey$$/firstName', function() {
+    rex = jt.regexFromPathPattern("/user/$$anyKey$$/firstName");
+    expect("/user/bobby/firstName".match(rex)).toBeTruthy();
+    expect("/notuser/bobby/firstName".match(rex)).toBeFalsy();
+    expect("/user/bobby/lastName".match(rex)).toBeFalsy();
+    expect("/user/bobby/nested/firstName".match(rex)).toBeFalsy();
+  });
+
+  it('$$anyKey$$ shouldn\'t match empty string', function() {
+    rex = jt.regexFromPathPattern("/user/$$anyKey$$/firstName");
+    expect("/user//firstName".match(rex)).toBeFalsy();
+    expect("/user/firstName".match(rex)).toBeFalsy();
+  });
+
+  it('should match multiple $$anyKey$$ in path', function() {
+    var rex = jt.regexFromPathPattern("/user/$$anyKey$$/friend/$$anyKey$$/firstName");
+    // match
+    expect("/user/bobby/friend/alice/firstName".match(rex)).toBeTruthy();
+    // no match
+    expect("/user/bobby/friend/alice/another".match(rex)).toBeFalsy();
+  });
+
+  it('should match /user/$$anyPath$$/lastName', function() {
+    var rex = jt.regexFromPathPattern("/user/$$anyPath$$/lastName");
+    // match
+    expect("/user/bobby/lastName".match(rex)).toBeTruthy();
+    expect("/user/bobby/friends/alice/lastName".match(rex)).toBeTruthy();
+    // no match
+    expect("/notuser/bobby/lastName".match(rex)).toBeFalsy();
+    expect("/bobby/lastName".match(rex)).toBeFalsy();
+    expect("/user/bobby/notLastName".match(rex)).toBeFalsy();
+  });
+
+  it('$$anyPath$$ should match empty string', function() {
+    var rex = jt.regexFromPathPattern("/user/$$anyPath$$/lastName");
+    expect("/user/lastName".match(rex)).toBeTruthy();
+  });
+
+  it('$$anyPath$$ can be the first thing in the path', function() {
+    var rex = jt.regexFromPathPattern("/$$anyPath$$/lastName");
+    expect("/lastName".match(rex)).toBeTruthy();
+    expect("/user/lastName".match(rex)).toBeTruthy();
+    expect("/user/bobby/lastName".match(rex)).toBeTruthy();
+  });
+
+  it('should match $$last$$ as is', function() {
+    var rex = jt.regexFromPathPattern("/user/bobby/friends/$$last$$/name");
+    // match
+    expect("/user/bobby/friends/$$last$$/name".match(rex)).toBeTruthy();
+    // no match
+    expect("/user/bobby/friends/$$last$$".match(rex)).toBeFalsy();
+    expect("/user/bobby/friends/$$last$$/another".match(rex)).toBeFalsy();
+  });
+
+  it('should match if $$anyPath$$ is last thing in the path', function() {
+    var rex = jt.regexFromPathPattern("/$$anyPath$$");
+    expect("/user/bobby/friends/$$last$$/name".match(rex)).toBeTruthy();
+    expect("/user/bobby/lastName".match(rex)).toBeTruthy();
+    expect("/lastName".match(rex)).toBeTruthy();
   });
 });
--- jasontree-core.js
+++ jasontree-core.js
@@ -11,16 +11,13 @@
     };
   };
 
-  exports.on = function(treeObj, evtName, listener) {
-    return treeObj.eventEmitter.on(evtName, listener);
-  };
-
-  exports.get = function(treeObj, path) {
+  function getOp(treeObj, path) {
     if (!_.isArray(path)) {
       path = pathElements(path);
     }
     return getFromSubtreePath(treeObj.tree, path);
   };
+  exports.get = getOp;
 
   function getNodeByKey(node, key) {
     if (!key || !node) {
@@ -50,46 +47,197 @@
     }
   };
 
-  exports.put = function(treeObj, path, value) {
+  exports.putOld = function(treeObj, path, value) {
     if (!_.isArray(path)) {
       path = pathElements(path);
     }
     var putOpPath = [];
-    var tec = new TreeEventCollector('treeNodeCreated', treeObj.eventEmitter, treeObj);
+    var tec = new TreeEventCollector('put', treeObj.eventEmitter, treeObj);
     treeObj.tree = putIntoTreeOnPath(treeObj.tree, path, putOpPath, value, tec.collect.bind(tec));
-    treeObj.putOpPath = putOpPath.reverse();
+    treeObj.putOpPath = putOpPath;
     tec.process(treeObj.doneNotifyingCb); // hack for testing ...
     return treeObj;
   };
+
+  exports.put = function(treeObj, path, value) {
+    path = interpolatedPathArray(path);
+    treeObj.tree = _.compose(
+      returnUpdatedTree,
+      emitNodeCreationEvents(treeObj.eventEmitter, treeObj.doneNotifyingCb),
+      listCreatedNodes(value),
+      insertAsLeaf(value),
+      createNewPath,
+      traverseExistingPath
+    )(requestedPathInTree(path, treeObj.tree));
+    treeObj.putOpPath = path;
+    return treeObj;
+  };
+
+  function tap(msg) {
+    return function(acc) {
+      console.log('tap', msg, acc);
+      return acc;
+    }
+  };
+
+  function requestedPathInTree(path, tree) {
+    return {
+      requestedPath: path,
+      newPath: path,
+      commonPath: [],
+      rootNode: tree,
+      lastExistingNode: tree,
+      lastNewNode: undefined,
+      earliestNewNode: undefined
+    };
+  };
+
+  function returnUpdatedTree(acc) {
+    return acc.rootNode;
+  };
+
+  function listCreatedNodes(value) {
+    return function(acc) {
+      var paths = [];
+      _.reduce(acc.newPath, function(path, pathKey) {
+        path.push(pathKey);
+        paths.push(_.clone(path));
+        return path;
+      }, _.clone(acc.commonPath));
+      acc.allCreatedPaths = paths.concat(enumeratePathsInObject(value, acc.requestedPath));
+      return acc;
+    };
+  };
+
+  function emitNodeCreationEvents(emitter, doneFn) {
+    return function(acc) {
+      if (emitter.listeners('put').length > 0) {
+        setTimeout(function() {
+          _.each(acc.allCreatedPaths, function(path) {
+            pathStr = '/' + path.join('/');
+            emitter.emit('put', pathStr);
+          });
+          if (doneFn) doneFn();
+        }, 1);
+      }
+      return acc;
+    };
+  };
+
+  function enumeratePathsInObject(value, pathSoFar) {
+    if (_.isArray(value) && value.length > 0) {
+      pathSoFar = pathSoFar.concat("$$last$$");
+      return [pathSoFar].concat(enumeratePathsInObject(_.last(value), pathSoFar));
+    }
+    else if (_.isObject(value)) {
+      return _.reduce(_.keys(value), function(paths, pathKey) {
+        var newPath = pathSoFar.concat(pathKey);
+        paths.push(newPath);
+        return paths.concat(enumeratePathsInObject(value[pathKey], newPath));
+      }, []);
+    }
+    else {
+      return [];
+    }
+  };
+
+  function traverseExistingPath(acc) {
+    return _.transform(acc.requestedPath, function(acc, pathKey) {
+      if (!_.isArray(acc.lastExistingNode) && !_.isObject(acc.lastExistingNode)) {
+        throw 'Can\'t traverse non-array non-object node';
+      }
+
+      var nextNode = undefined;
+
+      if (_.isArray(acc.lastExistingNode) && pathKey === '$$last$$' && acc.newPath.length > 1) {
+        nextNode = _.last(acc.lastExistingNode);
+      }
+      else if (_.isObject(acc.lastExistingNode)) {
+        nextNode = acc.lastExistingNode[pathKey];
+      }
+
+      if (nextNode) {
+        acc.newPath = _.tail(acc.newPath);
+        acc.commonPath.push(pathKey);
+        acc.lastExistingNode = nextNode;
+        return true;
+      }
+      else {
+        return false;
+      }
+    }, acc);
+  };
+
+  function createNewPath(acc) {
+    return _.reduce(_.initial(_.clone(acc.newPath).reverse()), function(acc, pathKey) {
+
+      var newNode = (pathKey === '$$last$$') ? [] : {};
+
+      if (acc.earliestNewNode) {
+        if (pathKey === '$$last$$') {
+          newNode.push(acc.earliestNewNode);
+        }
+        else {
+          newNode[pathKey] = acc.earliestNewNode;
+        }
+        acc.earliestNewNode = newNode;
+      }
+      else {
+        acc.earliestNewNode = newNode;
+        acc.lastNewNode = newNode;
+      }
+
+      return acc;
+    }, acc);
+  };
+
+  function insertAsLeaf(value) {
+    return function(acc) {
+      var lastNode = acc.lastNewNode || acc.lastExistingNode || acc.rootNode;
+      var lastKey = _.last(acc.requestedPath);
+      nodeInsert(lastNode, lastKey, value);
+      if (acc.lastExistingNode && acc.earliestNewNode) {
+        nodeInsert(acc.lastExistingNode, _.first(acc.newPath), acc.earliestNewNode);
+      }
+      return acc;
+    }
+  }
+
+  function nodeInsert(node, key, value) {
+    if (_.isArray(node) && key === '$$last$$') {
+      node.push(value);
+    }
+    else if (_.isObject(node)) {
+      node[key] = value;
+    }
+    else {
+      throw 'Cannot insert value into non-object node or not-last-position-in-array';
+    }
+  }
 
   exports.lastPutPath = function(treeObj) {
     return treeObj.putOpPath;
   };
 
   function TreeEventCollector(evtName, eventEmitter, treeObj) {
-    var events = [];
+    var paths = [];
 
     this.collect = function(pathElements) {
-      events.push(_.clone(pathElements));
+      paths.push(_.clone(pathElements));
     };
 
     this.process = function(done) {
-      //console.log("notifying listeners");
       setTimeout(function() {
         notifyListeners();
-        if (done) {
-          done();
-        }
+        if (done) done();
       }, 1);
     };
 
     function notifyListeners() {
-      //console.log("TIMEOUT FIRED");
       if (eventEmitter.listeners(evtName).length > 0) {
-        //console.log("got listeners");
-        _.each(events, function(evt) {
-          //console.log("emitting", evtName, evt);
-          eventEmitter.emit(evtName, evt);
+        _.each(paths, function(path) {
+          pathStr = '/' + path.join('/');
+          eventEmitter.emit(evtName, pathStr);
         });
       }
     };
@@ -98,18 +246,25 @@
   function putIntoTreeOnPath(container, path, putOpPath, value, createdCb) {
     var key = _.head(path);
     if (path.length == 1) {
+      if (putOpPath) putOpPath.push(key);
       return putValueOnPathKey(container, key, value, putOpPath, createdCb);
     }
     else {
       var subContainer = getNextSubcontainerNode(container, key);
       if (subContainer) {
+        if (putOpPath) {
+          putOpPath.push(key);
+        }
         putIntoTreeOnPath(subContainer, _.tail(path), putOpPath, value, createdCb);
-        putOpPath.push(key);
         return container;
       }
       else {
+        if (putOpPath) {
+          putOpPath.push(key);
+        }
         subContainer = createNextSubcontainerNode(container, path);
-        subContainer = putIntoTreeOnPath(subContainer, _.tail(path), putOpPath, value, createdCb);
+        // stop propagating putOpPath
+        subContainer = putIntoTreeOnPath(subContainer, _.tail(path), undefined, value, createdCb);
         return putValueOnPathKey(container, key, subContainer, putOpPath, createdCb);
       }
     }
@@ -122,7 +277,7 @@
       }
     }
     else if (_.isObject(container) && !_.isArray(container)) {
-      if (key !== '$$uuid$$' && container[key]) {
+      if (container[key]) {
         return container[key];
       }
     }
@@ -149,9 +304,6 @@
       }
     }
     else if (_.isObject(container)) {
-      if (key === '$$uuid$$') {
-        key = uuid.v4();
-      }
       if (!container[key]) {
         container[key] = value;
       }
@@ -163,7 +315,6 @@
       throw 'Cannot insert value into a non-container node ' + container;
     }
 
-    putOpPath.push(key);
     createdCb(putOpPath);
     recurseCreatedCb(value, createdCb, putOpPath);
 
@@ -187,6 +338,43 @@
 
   function pathElements(path) {
     var elements = path.split('/');
-    return _.reject(elements, _.isEmpty);
+    return _(elements)
+      .reject(_.isEmpty)
+      .map(function(e) {
+        return (e === '$$uuid$$') ? uuid.v4() : e;
+      })
+      .value();
+  };
+
+  function interpolatedPathArray(path) {
+    var elements = _.isArray(path) ? path : path.split('/');
+    return _(elements)
+      .reject(_.isEmpty)
+      .map(function(e) {
+        return (e === '$$uuid$$') ? uuid.v4() : e;
+      })
+      .value();
+  };
+
+  function regexFromPathPattern(pattern) {
+    pattern = pattern.replace(/\$\$anyKey\$\$/g, '([^/]+?)');
+    pattern = pattern.replace(/\$\$anyPath\$\$\//g, '((.+)/)*');
+    pattern = pattern.replace(/\$\$anyPath\$\$$/, '((.+)/)*([^/]+?)');
+    pattern = pattern.replace(/\$\$last\$\$/g, '\\$\\$last\\$\\$');
+    pattern = '^' + pattern + '$';
+    return pattern;
+  }
+  // what's a better way to expose this for testing? class? module?
+  exports.regexFromPathPattern = regexFromPathPattern;
+
+  exports.on = function(treeObj, evtName, pattern, callback) {
+    if (evtName !== 'put') throw 'Can only listen on \'put\' events';
+    var rex = regexFromPathPattern(pattern);
+    var listener = function(path) {
+      if (path.match(rex)) {
+        callback(path, getOp(treeObj, path));
+      }
+    }
+    treeObj.eventEmitter.on(evtName, listener);
   };
 })(exports);
